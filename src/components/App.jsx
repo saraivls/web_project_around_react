52a13669507a9b6e17c8daa1ded39f3112fc8621
SaraiVelasquez <saraivelasquez58@gmail.com>
2025-11-26 00:08:30 -0500

    codigo_migrado_a_nuevos_archivos_footer_main_header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,16 @@
 
 
-import avatarImg from "../images/Avatar.png";
-import pencilImg from "../images/pencil_act_profile.png";
+
+import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Main from "./Main/Main";
 
 
 function App() {
   return (<div className="page">
      <Header />
-    <main className="content">
-        <div className="profile">
-          <div className="profile__cont"><img
-            className="profile__avatar"
-            src={avatarImg}
-            alt="foto de avatar"
-          />
-           <img
-              className="profile__avatar-pencil"
-              src={pencilImg}
-              alt=" Editar avatar"
-            />
-          </div>
-          <div className="profile__info">
-            <div className="profile__details">
-                <h1 className="profile__name">Jacques Cousteau</h1>
-                <button className="button" id="edit-button">
-                    <img
-                      className="button__image"
-                      src={pencilImg}
-                      alt="button"
-                    />
-                </button>
-            </div>
-            <p className="profile__text">Explorador</p>
-          </div>
-          <button className="add-button" id="add-card">
-            <img className="add-button__image" 
-            src="./images/sumar.png" 
-            alt="agregar tarjeta" 
-            />
-          </button>          
-        </div>
-        <section className="gallery">
-            <div className="gallery__cards">
-                    
-            </div>
-        </section>
-      </main>
-      <footer className="footer">© 2024 Around The U.S.</footer>
+     <Main />
+    <Footer />     
     </div>
   )
 }
