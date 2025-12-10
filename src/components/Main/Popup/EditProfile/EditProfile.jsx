a1017afd29a132f403b0fcc4759af7682afdd3db
SaraiVelasquez <saraivelasquez58@gmail.com>
2025-12-10 18:47:25 -0500

    archivo_EditProfile_creado

---
+++ src/components/Main/Popup/EditProfile/EditProfile.jsx
@@ -0,0 +1,34 @@
+const EditProfile = () => {
+    return (
+        <form method="post" className="popup__form">
+            <input
+              type="text"
+              id="name-profile"
+              value=""
+              name="name"
+              placeholder="Jacques Cousteau"
+              className="popup__input popup__input_name"
+              minLength="2"
+              maxLength="30"
+              required
+            />
+            <span className="popup__error" id="name-profile-error"></span>
+            <input
+              type="text"
+              id="job-profile"
+              value=""
+              name="about"
+              placeholder="Explorador"
+              className="popup__input popup__input_description"
+              minLength="2"
+              maxLength="30"
+              required
+            />
+            <span className="popup__error" id="job-profile-error"></span>
+            <button type="submit" className="submit" id="save-button">
+              <span className="submit__text">Guardar</span>
+            </button>
+          </form>  
+    );
+};
+export default EditProfile;
