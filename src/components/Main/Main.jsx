1d28d5280a7df58d238e6b434eb5c3f763b82355
SaraiVelasquez <saraivelasquez58@gmail.com>
2025-12-10 18:45:09 -0500

    cards_importadas

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,40 @@
 import avatarImg from "../../images/Avatar.png";
 import pencilImg from "../../images/pencil_act_profile.png";
+import plusImg from "../../images/sumar.png";
+import Card from "./Card/Card.jsx";
 
-const Main = () => {   
+
+const cards = [
+  {
+    isLiked: false,
+    _id: '5d1f0611d321eb4bdcd707dd',
+    name: 'Yosemite Valley',
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg',
+    owner: '5d1f0611d321eb4bdcd707dd',
+    createdAt: '2019-07-05T08:10:57.741Z',
+  },
+  {
+    isLiked: false,
+    _id: '5d1f064ed321eb4bdcd707de',
+    name: 'Lake Louise',
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg',
+    owner: '5d1f0611d321eb4bdcd707dd',
+    createdAt: '2019-07-05T08:11:58.324Z',
+  },
+]; 
+
+
+const Main = (props) => {   
+  const {
+    onClickEditProfile, 
+    onClickEditAvatar, 
+    onClickAddCard,
+    onClickCard} = props;
+
     return (
         <main className="content">
         <div className="profile">
-          <div className="profile__cont"><img
+          <div className="profile__cont" onClick= {onClickEditAvatar}><img
             className="profile__avatar"
             src={avatarImg}
             alt="foto de avatar"
@@ -13,7 +42,7 @@ const Main = () => {
            <img
               className="profile__avatar-pencil"
               src={pencilImg}
-              alt=" Editar avatar"
+              alt=" Editar avatar"   
             />
           </div>
           <div className="profile__info">
@@ -24,6 +53,7 @@ const Main = () => {
                       className="button__image"
                       src={pencilImg}
                       alt="button"
+                      onClick= {onClickEditProfile}
                     />
                 </button>
             </div>
@@ -31,14 +61,17 @@ const Main = () => {
           </div>
           <button className="add-button" id="add-card">
             <img className="add-button__image" 
-            src="./images/sumar.png" 
+            src={plusImg} 
             alt="agregar tarjeta" 
+            onClick= {onClickAddCard}
             />
           </button>          
         </div>
         <section className="gallery">
             <div className="gallery__cards">
-                    
+                    {cards.map((card) => (
+                      <Card key={card._id} card={card} onClick ={onClickCard}/>
+                    ))}                        
             </div>
         </section>
       </main>
